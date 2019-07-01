9af2194014b5aa4747505fcc448372355086a4dc
Aaron <nordin.aaron@gmail.com>
2019-07-01 16:46:58 -0600

    done

--- Part1/index.js
+++ Part1/index.js
@@ -1,77 +1,89 @@
-const board = [];
-const topLeft = board[0];
-const topMiddle = board[1];
-const topRight = board[2];
-const midLeft = board[3];
-const midMiddle = board[4];
-const midRight = board[5];
-const botLeft = board[6];
-const botMiddle = board[7];
-const botRight = board[8];
-let boardFull = true;
+let board = [];
 
 function play(clickedId) {
+  // console.log(clickedId)
   let player = document.getElementById("player");
   let clickedEle = document.getElementById(clickedId);
-
-  if (player === "X" && board[clickedId] === undefined) {
-    play.innerText = "O";
+  //   console.log(clickedEle)
+  //   console.log(board[clickedId])
+  //   console.log(player)
+  if (player.innerText === "X" && !board[clickedId]) {
+    player.innerText = "O";
     clickedEle.innerText = "X";
     board[clickedId] = "X";
-  } else if (player === "O" && board[clickedId] === undefined) {
+    // console.log(board[clickedId])
+  } else if (player.innerText === "O" && !board[clickedId]) {
     player.innerText = "X";
     clickedEle.innerText = "O";
     board[clickedId] = "O";
-  } else alert("You cannot click here!")
-}
+    // console.log(board[clickedId])
+  } else alert("You cannot click here!");
 
-if (topLeft !== undefined && topLeft === topMiddle && topLeft === topRight) {
-  alert(`${topLeft} is the winner!`);
-  blackDiamond();
-}
-if (midLeft !== undefined && midLeft === midMiddle && midLeft === midRight) {
-  alert(`${midLeft} is the winner!`);
-  blackDiamond();
-}
-if (botLeft !== undefined && botLeft === botMiddle && botLeft === botRight) {
-  alert(`${botLeft} is the winner!`);
-  blackDiamond();
-}
-if (topLeft !== undefined && topLeft === midLeft && topLeft === botLeft) {
-  alert(`${topLeft} is the winner!`);
-  blackDiamond();
-}
-if (
-  topMiddle !== undefined &&
-  topMiddle === midMiddle &&
-  topMiddle === botMiddle
-) {
-  alert(`${topMiddle} is the winner!`);
-  blackDiamond();
-}
-if (topRight !== undefined && topRight === midRight && topRight === botRight) {
-  alert(`${topRight} is the winner!`);
-  blackDiamond();
-}
-if (topLeft !== undefined && topLeft === midMiddle && topLeft === botRight) {
-  alert(`${topLeft} is the winner!`);
-  blackDiamond();
-}
-if (botLeft !== undefined && botLeft === midMiddle && botLeft === topRight) {
-  alert(`${botLeft} is the winner!`);
-  blackDiamond();
-}
+  let topLeft = board[0];
+  let topMiddle = board[1];
+  let topRight = board[2];
+  let midLeft = board[3];
+  let midMiddle = board[4];
+  let midRight = board[5];
+  let botLeft = board[6];
+  let botMiddle = board[7];
+  let botRight = board[8];
 
-for (let i = 0; i <= 8; i++) {
-  board[i] === undefined ? (boardFull = false) : null;
-}
-if (boardFull === true) {
-  alert("Cat's game!");
-  blackDiamond();
-}
-
-function blackDiamond() {
-  for (let key in board) {
-    board[key] = undefined;
+  if (topLeft !== undefined && topLeft === topMiddle && topLeft === topRight) {
+    alert(`${topLeft} is the winner!`);
+    reset();
   }
+  if (midLeft !== undefined && midLeft === midMiddle && midLeft === midRight) {
+    alert(`${midLeft} is the winner!`);
+    reset();
+  }
+  if (botLeft !== undefined && botLeft === botMiddle && botLeft === botRight) {
+    alert(`${botLeft} is the winner!`);
+    reset();
+  }
+  if (topLeft !== undefined && topLeft === midLeft && topLeft === botLeft) {
+    alert(`${topLeft} is the winner!`);
+    reset();
+  }
+  if (
+    topMiddle !== undefined &&
+    topMiddle === midMiddle &&
+    topMiddle === botMiddle
+  ) {
+    alert(`${topMiddle} is the winner!`);
+    reset();
+  }
+  if (
+    topRight !== undefined &&
+    topRight === midRight &&
+    topRight === botRight
+  ) {
+    alert(`${topRight} is the winner!`);
+    reset();
+  }
+  if (topLeft !== undefined && topLeft === midMiddle && topLeft === botRight) {
+    alert(`${topLeft} is the winner!`);
+    reset();
+  }
+  if (botLeft !== undefined && botLeft === midMiddle && botLeft === topRight) {
+    alert(`${botLeft} is the winner!`);
+    reset();
+  }
+
+  let boardFull = true;
+  for (let i = 0; i <= 8; i++) {
+    board[i] === undefined ? (boardFull = false) : null;
+  }
+  if (boardFull === true) {
+    alert("Cat's game!");
+    reset();
+  }
+}
+
+function reset() {
+  document.getElementsByClassName("row").forEach(element => {
+    element.innerText = "";
+    element.innerHTML = "";
+  });
+  board = [];
 }
